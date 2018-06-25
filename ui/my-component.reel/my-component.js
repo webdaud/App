5cf04f705316fd7000a51063fc93dc20fefedbd8
webdaud <noreply>
2018-06-25 11:19:12 +0000

    Add component my-component

---
+++ ui/my-component.reel/my-component.js
@@ -0,0 +1,12 @@
+/**
+ * @module "ui/my-component.reel"
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class MyComponent
+ * @extends Component
+ */
+exports.MyComponent = Component.specialize(/** @lends MyComponent.prototype */{
+
+});
